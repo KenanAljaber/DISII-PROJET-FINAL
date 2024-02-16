5787850427d44e43f390df0d3642bf6703a16ed7
morristewelde <morris.sleshi@gmail.com>
2024-02-16 16:12:20 +0100

    "AdminController with showUsers"

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminController extends AbstractController
 {
@@ -21,6 +23,16 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/ajouteFormateur.html.twig', [
   
+        ]);
+    }
+
+    #[Route('/admin/showusers', name: 'app_admin_showusers')]
+    public function showUsers(EntityManagerInterface $entityManager): Response
+    {
+        $userRepo = $entityManager->getRepository(User::class);
+        $users = $userRepo->findAll();
+        return $this->render('admin/userShow.html.twig', [
+            'users' => $users
         ]);
     }
 }
